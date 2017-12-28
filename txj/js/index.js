49db81db69890f2358a6423756ad0523cb61da06
梁强 <ty7030236@sina.com>
2017-12-28 10:54:15 +0800

    e
    e

--- txj/js/index.js
+++ txj/js/index.js
@@ -1,31 +1,49 @@
-
+// 轮播图
 {
-    let dot = $(".hd ul li")
-    let banner = $(".bd ul li")
-    dot.mouseover(function () {
-        dot.css({background: "#000", opacity: 0.7})
-            .filter(this).css({
-            background: "#fff"
-        })
-        var index = $(this).index();
-        banner.css("opacity", "0").eq(index).css("opacity", "1")
-    })
+    const bannerbox = document.querySelector(".bd");
+    // console.log(bannerbox);
+    const banners = document.querySelectorAll(".bd ul li");
+    // console.log(banners);
+    const sliders = document.querySelectorAll(".hd ul li");
+    // console.log(sliders);
 
-    let num = 0
-    function move() {
+    sliders.forEach(function (ele, index) {
+        ele.onmouseover = function () {
+            for (var i = 0; i < sliders.length; i++) {
+                sliders[i].classList.remove("active");
+                banners[i].classList.remove("active");
+            }
+            // 清除类名
+            this.classList.add("active");
+            banners[index].classList.add("active");
+            num = index
+            // 添加类名
+        }
+    });
+
+    let num = 0;
+    let move = function () {
         num++;
-        if (num === dot.length) {
+        if (num === sliders.length) {
             num = 0
         }
-        dot.filter(".active").removeClass("active").end().eq(num).addClass("active")
-        banner.filter(".active").removeClass("active").fadeOut(200).end().eq(num).addClass("active").fadeIn(500);
-    }
+        if (num === -1) {
+            num = sliders.length - 1;
+        }
+        // 判断
+        for (var i = 0; i < sliders.length; i++) {
+            sliders[i].classList.remove("active");
+            banners[i].classList.remove("active");
+        }
+        sliders[num].classList.add("active");
+        banners[num].classList.add("active");
+    };
 
-    var st = setInterval(move, 3000)
-    $(".bd").mouseover(function () {
+    let st = setInterval(move, 3000);
+    bannerbox.onmouseover = function () {
         clearInterval(st)
-    })
-    $(".bd").mouseout(function () {
+    };
+    bannerbox.onmouseout = function () {
         st = setInterval(move, 3000)
-    })
+    };
 }
