e1289956ca2eafb6effb6013733cd6e1ce261a82
梁强 <ty7030236@sina.com>
2017-12-29 09:04:55 +0800

    finish
    finish

--- sofa/js/sofa.js
+++ sofa/js/sofa.js
@@ -1,12 +1,12 @@
 // banner效果
 {
-    const banners = document.querySelectorAll(".banner-image-mask>img")
-    const bannerbox = document.querySelector(".banner-image-mask")
+    const banners = document.querySelectorAll(".banner-image-mask>img");
+    const bannerbox = document.querySelector(".banner-image-mask");
     // console.log(banners,bannerbox)
-    let num = 0
+    let num = 0;
     let move = function () {
         num++;
-        if (num == 2) {
+        if (num === 2) {
             num = 0
         }
         ;
@@ -20,14 +20,14 @@
 
 // 电视应用效果
 {
-    const marks = document.querySelectorAll(".app-block-element-mask")
+    const marks = document.querySelectorAll(".app-block-element-mask");
     marks.forEach(function (ele, index) {
         ele.onmouseover = function () {
-            marks[index].style.opacity = "1"
+            marks[index].style.opacity = "1";
             marks[index].style.transition = ".2s all"
-        }
+        };
         ele.onmouseout = function () {
-            marks[index].style.opacity = "0"
+            marks[index].style.opacity = "0";
             marks[index].style.transition = ".2s all"
         }
     })
@@ -35,12 +35,12 @@
 
 // 导航栏下拉效果
 {
-    const lis = document.querySelectorAll(".nav-one > li")
-    console.log(lis)
-    const sec = document.querySelectorAll(".dropdown-menu")
-    console.log(sec)
-    // 一般方法
-    const heightarr = []
+    const lis = document.querySelectorAll(".nav-one > li");
+    console.log(lis);
+    const sec = document.querySelectorAll(".dropdown-menu");
+    console.log(sec);
+    // 一般方法;
+    const heightarr = [];
     sec.forEach(function (ele) {
         let height = ele.querySelectorAll("li").length * 30;
         heightarr.push(height)
@@ -51,7 +51,7 @@
     lis.forEach(function (ele, index) {
         ele.onmouseover = function () {
             sec[index].style.height = heightarr[index] + "px"
-        }
+        };
         ele.onmouseout = function () {
             sec[index].style.height = 0
         }
--- xiaomi/js/xiaomi.js
+++ xiaomi/js/xiaomi.js
@@ -1,31 +1,30 @@
 // 轮播图
 {
-    const bannerbox = document.querySelector(".banner-box")
+    const bannerbox = document.querySelector(".banner-box");
     // console.log(bannerbox)
-    const banners = document.querySelectorAll(".banner-box a img")
+    const banners = document.querySelectorAll(".banner-box a img");
     // console.log(banners)
-    const sliders = document.querySelectorAll(".lunbodian-box div a")
+    const sliders = document.querySelectorAll(".lunbodian-box div a");
     // console.log(sliders)
 
     sliders.forEach(function (ele, index) {
         ele.onmouseover = function () {
             for (var i = 0; i < sliders.length; i++) {
-                sliders[i].classList.remove("active")
+                sliders[i].classList.remove("active");
                 banners[i].classList.remove("active");
             }
             // 清除类名
             this.classList.add("active");
-            banners[index].classList.add("active")
+            banners[index].classList.add("active");
             num = index
             // 添加类名
         }
-    })
+    });
 
-
-    let num = 0
+    let num = 0;
     let move = function () {
         num++;
-        if (num == sliders.length) {
+        if (num === sliders.length) {
             num = 0
         }
         // 判断
@@ -33,19 +32,19 @@
             sliders[i].classList.remove("active");
             banners[i].classList.remove("active");
         }
-        sliders[num].classList.add("active")
+        sliders[num].classList.add("active");
         banners[num].classList.add("active")
     };
 }
 
 // 选项卡
 {
-    const banners = document.querySelectorAll(".banner-box a img")
-    const sliders = document.querySelectorAll(".lunbodian-box div a")
-    const bannerbox = document.querySelector(".banner-box")//获取单个元素.bannerbox
-    const prev = document.querySelector(".xuanxiangka-left")
+    const banners = document.querySelectorAll(".banner-box a img");
+    const sliders = document.querySelectorAll(".lunbodian-box div a");
+    const bannerbox = document.querySelector(".banner-box");//获取单个元素.bannerbox
+    const prev = document.querySelector(".xuanxiangka-left");
     // console.log(prev)
-    const next = document.querySelector(".xuanxiangka-right")
+    const next = document.querySelector(".xuanxiangka-right");
     // console.log(next)
     sliders.forEach(function (ele, index) {   //ele代表的  dianlist    index索引值
         ele.onmouseover = function () {
@@ -58,33 +57,32 @@
             // bannerbox.style.background = colorarr[index]
             num = index
         }
-    })
+    });
 
     //自动切换轮播图
-    let num = 0
+    let num = 0;
     let move = function () {
         num++;
-        if (num == sliders.length) {
+        if (num === sliders.length) {
             num = 0
         }
-        ;
-        if (num == -1) {
+        if (num === -1) {
             num = sliders.length - 1;
         }
         for (var i = 0; i < sliders.length; i++) {
             sliders[i].classList.remove("active");//移除类名
             banners[i].classList.remove("active");//移除类名
         }
-        sliders[num].classList.add("active")//当前对应的加上类名
-        banners[num].classList.add("active")//当前对应的加上类名
+        sliders[num].classList.add("active"); //当前对应的加上类名
+        banners[num].classList.add("active"); //当前对应的加上类名
     };
-    let st = setInterval(move, 3000)
+    let st = setInterval(move, 3000);
     bannerbox.onmouseover = function () {
         clearInterval(st)
-    }
+    };
     bannerbox.onmouseout = function () {
         st = setInterval(move, 3000)
-    }
+    };
     next.onclick = function () {
         move();
     };
@@ -97,64 +95,64 @@
 
 // 内容选项卡
 {
-    let bannerbox = document.querySelectorAll(".neirong-box>div ")
+    let bannerbox = document.querySelectorAll(".neirong-box>div ");
     // console.log(bannerbox)
-
-    function xiaomibanner(bannerbox) {//此处封装以下内容
-        let banner = bannerbox.querySelector(".neirong-box >div .neirong-box-one-inner")
+    function xiaomibanner(bannerbox) {
+        //此处封装以下内容
+        let banner = bannerbox.querySelector(".neirong-box >div .neirong-box-one-inner");
         // console.log(banner)
-        let dians = bannerbox.querySelectorAll(".neirong-box>div .neirong-dot")
+        let dians = bannerbox.querySelectorAll(".neirong-box>div .neirong-dot");
         // console.log(dians)
-        let next = bannerbox.querySelector(".neirong-box>div .neirong-xxk-right-box")
+        let next = bannerbox.querySelector(".neirong-box>div .neirong-xxk-right-box");
         // console.log(next)
-        let prev = bannerbox.querySelector(".neirong-box >div .neirong-xxk-left-box")
+        let prev = bannerbox.querySelector(".neirong-box >div .neirong-xxk-left-box");
         // console.log(prev)
-        let now = 0
+        let now = 0;
 
         // console.log(now)
         //点击圆点的时候图片的切换
         dians.forEach(function (ele, index) {
             ele.onclick = function () {
                 let left = -index * 296;
-                banner.style.left = left + "px"
+                banner.style.left = left + "px";
                 dians[now].style.background = "#B0B0B0";
-                dians[now].style.border = "2px solid #fff"
-                this.style.background = "#fff"
-                this.style.border = "2px solid #FF6700"
+                dians[now].style.border = "2px solid #fff";
+                this.style.background = "#fff";
+                this.style.border = "2px solid #FF6700";
                 now = index;
             }
-        })
+        });
         //以下是点击左右箭头的时候，点颜色的变化和图片的切换
         next.onclick = function () {
             if (now === dians.length - 1) {
                 return
             }
-            dians[now].style.background = "#B0B0B0"
-            dians[now].style.border = "2px solid #fff"
+            dians[now].style.background = "#B0B0B0";
+            dians[now].style.border = "2px solid #fff";
             now++;
-            banner.style.left = -now * 296 + "px"
+            banner.style.left = -now * 296 + "px";
             dians[now].style.background = "#fff";
             dians[now].style.border = "2px solid #FF6700"
-        }
+        };
         prev.onclick = function () {
             if (now === 0) {//左右箭头点击点击到最后一张的时候就点不动不能点了
                 return;
             }
             dians[now].style.background = "#B0B0B0";
-            dians[now].style.border = "2px solid #fff"
+            dians[now].style.border = "2px solid #fff";
             now--;
-            banner.style.left = -now * 296 + "px"
+            banner.style.left = -now * 296 + "px";
             dians[now].style.background = "#fff";
             dians[now].style.border = "2px solid #FF6700"
-        }
+        };
         //下面是鼠标经过的时候左右箭头显示，离开的时候左右箭头隐藏
         bannerbox.onmouseover = function () {
-            prev.style.opacity = "0.7"
+            prev.style.opacity = "0.7";
             next.style.opacity = "0.7"
             //opacity中1是显示0是隐藏
-        }
+        };
         bannerbox.onmouseout = function () {
-            prev.style.opacity = "0"
+            prev.style.opacity = "0";
             next.style.opacity = "0"
             //opacity中1是显示0是隐藏
         }
@@ -168,22 +166,21 @@
 
 // 小米明星单品
 {
-    let banner = document.querySelector(".xiaomi-bgbox-inner")
+    let banner = document.querySelector(".xiaomi-bgbox-inner");
     // console.log(banner)
-    let next = document.querySelector(".xiaomi-star-xxk-right")
+    let next = document.querySelector(".xiaomi-star-xxk-right");
     // console.log(next)
-    let prev = document.querySelector(".xiaomi-star-xxk-left")
+    let prev = document.querySelector(".xiaomi-star-xxk-left");
     // console.log(prev)
-    let now = 0
+    let now = 0;
     // console.log(now)
-
     next.onclick = function () {
         if (now === 1) {
             return
         }
         now++;
         banner.style.left = -now * 1200 + "px"
-    }
+    };
     prev.onclick = function () {
         if (now === 0) {//左右箭头点击点击到最后一张的时候就点不动不能点了
             return;
